e00de155a2ee9b66be74fa996b84a08d84a9dae5
shammah <shammahk1@gmail.com>
2019-10-24 16:44:20 +0100

    fixed task 7

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,7 +6,7 @@ import "rxjs/add/operator/map" ;
 export class ProductService {
   private _albumUrl = "../assets/album.json";
 
-  constructor(private _http:Http) {  }
+  constructor(private _http:Http) { }
 
   getAlbum(id: number) {
     return this._http.get(this._albumUrl).map((response) => {
