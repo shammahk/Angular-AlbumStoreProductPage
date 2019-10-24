37511ddb6d4b39ca8530bdca562e37280959cb91
shammah <shammahk1@gmail.com>
2019-10-24 19:45:44 +0100

    added tracklisting component

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from "../product.service";
+import { Album } from "../album";
+
 
 @Component({
   selector: 'app-product-page',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
+  albumInfo:Album;
 
-  constructor() { }
+  constructor(private _productService : ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
