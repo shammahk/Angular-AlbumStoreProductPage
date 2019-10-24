cd90c23a9a223f13b9bdc04bb62b7b6cd17e0f47
shammah <shammahk1@gmail.com>
2019-10-24 17:28:10 +0100

    fixed getAlbum

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,9 +8,8 @@ export class ProductService {
 
   constructor(private _http:Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => {
-      response.json() });
+  getAlbum(id:number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
